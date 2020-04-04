f9d42a5dab175877974e1b2174036ce451023f68
Alvaro <Alvaro@192.168.1.138>
2020-04-04 17:45:52 +0200

    Origen

--- src/grupo9/Grupo9.java
+++ src/grupo9/Grupo9.java
@@ -15,7 +15,7 @@ public class Grupo9 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hola gente descargate el lol justo");
     }
     
 }
